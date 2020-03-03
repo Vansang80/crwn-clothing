9fecb13ebcf0b40f6a699a40c2d3cb5835301c37
Pascal Nguyen <pasngu@student.42.fr>
2020-03-03 17:01:05 +0100

    upddate shop item

--- src/App.js
+++ src/App.js
@@ -3,18 +3,8 @@ import { Route } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component'
-
-
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  )
-}
-
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'; 
 
 
 
@@ -23,7 +13,7 @@ function App() {
   return (
     <div>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
     </div>
   );
 }
